dd4a54a86463e1616b4eb94ce66ae4acf7babe4d
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-11-07 10:20:36 +0100

    chore: project init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'red-line',
+  webDir: 'dist',
+}
+
+export default config
