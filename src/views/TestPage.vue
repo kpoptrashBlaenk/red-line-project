3e5d0d972ff446a5e11062c5c6b5a61699c039a4
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-11-11 02:55:12 +0100

    feat(design): create cyna solid button

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <div class="ion-padding">
+    <ion-button color="secondary" mode="md">
+      <ion-icon slot="end" :icon="chevronForwardOutline"></ion-icon>
+      Custom Cyna Button
+    </ion-button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { IonButton, IonIcon } from '@ionic/vue'
+import { chevronForwardOutline } from 'ionicons/icons'
+</script>
