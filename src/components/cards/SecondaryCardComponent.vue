f629c5c0b9df2ceac8eb3b60a2728171e0e946b4
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-11-12 14:02:51 +0100

    feat(design): add basic card

---
+++ src/components/cards/SecondaryCardComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <IonCard>
+    <IonCardHeader v-if="title" color="secondary">
+      <!-- Title -->
+      <IonCardTitle>
+        <div class="flex items-center justify-center text-2xl font-semibold p-4">
+          <IonIcon v-if="icon" :icon class="text-4xl me-2"></IonIcon>
+          <div>{{ title }}</div>
+        </div>
+      </IonCardTitle>
+    </IonCardHeader>
+
+    <IonCardContent>
+      <div class="p-10">
+        <!-- Subtitle -->
+        <div v-if="subtitle" class="text-xl font-extrabold mb-5">
+          {{ subtitle }}
+        </div>
+
+        <!-- Content -->
+        <div v-if="content">
+          {{ content }}
+        </div>
+
+        <!-- Content List -->
+        <ul v-if="list" class="list-disc font-semibold text-base pl-5 mt-4">
+          <li v-for="(item, index) in list" :key="index" class="mb-2.5">
+            {{ item }}
+          </li>
+        </ul>
+      </div>
+    </IonCardContent>
+  </IonCard>
+</template>
+
+<script setup lang="ts">
+/* Imports */
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonIcon } from '@ionic/vue'
+
+/* Props */
+defineProps<{
+  title?: string
+  icon?: string
+  subtitle?: string
+  content?: string
+  list?: string[]
+}>()
+</script>
+
+<style lang="css" scoped>
+ion-card-content {
+  color: var(--ion-text-color);
+}
+</style>
--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="ion-padding"></div>
+  <div class="p-10 mx-30 grid grid-cols-2 gap-10">
+    <SecondaryCardComponent
+      title="Protection SOC 24/7"
+      :icon="shieldCheckmarkOutline"
+      subtitle="PME et ETI, protégez ce qui est essentiel pour votre entreprise"
+      :list="[
+        'Protégez votre environnement de travail (EDR et Digital Workplace) : postes, téléphones, serveurs, utilisateurs.',
+        'Notre équipe de SOC analyste prend en charge la : détection, analyse et réponse aux incidents.',
+      ]"
+    />
+
+    <SecondaryCardComponent
+      title="Protection SOC 24/7"
+      :icon="shieldCheckmarkOutline"
+      subtitle="PME et ETI, protégez ce qui est essentiel pour votre entreprise"
+      :list="[
+        'Protégez votre environnement de travail (EDR et Digital Workplace) : postes, téléphones, serveurs, utilisateurs.',
+        'Notre équipe de SOC analyste prend en charge la : détection, analyse et réponse aux incidents.',
+      ]"
+    />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SecondaryCardComponent from '@/components/cards/SecondaryCardComponent.vue'
+import { shieldCheckmarkOutline } from 'ionicons/icons'
+</script>
