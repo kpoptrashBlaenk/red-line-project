dc9d6b12c880d850c2c91db284a6e28b894915e0
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-11-24 15:45:33 +0100

    style(home): add animation to header

--- src/components/layouts/default/DefaultLayout.vue
+++ src/components/layouts/default/DefaultLayout.vue
@@ -4,9 +4,9 @@
 
     <DefaultModalLayout />
 
-    <DefaultHeaderLayout />
+    <DefaultHeaderLayout :is-scrolled />
 
-    <IonContent id="main-content" fullscreen>
+    <IonContent id="main-content" ref="content" fullscreen scroll-events @ion-scroll="handleScroll">
       <slot></slot>
 
       <DefaultFooter v-if="isDesktop()" class="mt-10" />
@@ -18,8 +18,17 @@
 /* Imports */
 import isDesktop from '@/utils/isDesktop'
 import { IonContent, IonPage } from '@ionic/vue'
+import { ref } from 'vue'
 import DefaultFooter from '../footer/DefaultFooter.vue'
 import DefaultHeaderLayout from './DefaultHeaderLayout.vue'
 import DefaultMenuLayout from './DefaultMenuLayout.vue'
 import DefaultModalLayout from './DefaultModalLayout.vue'
+
+/* Refs */
+const isScrolled = ref<boolean>(false)
+
+/* Functions */
+function handleScroll(event: CustomEvent) {
+  isScrolled.value = event.detail.currentY !== 0
+}
 </script>
--- src/components/ui/LogoComponent.vue
+++ src/components/ui/LogoComponent.vue
@@ -1,10 +1,18 @@
 <template>
   <RouterLink to="/home" class="inline-block">
-    <IonImg src="https://cyna-it.fr/wp-content/themes/theme-cyna-it/images/logo-cyna-white.svg" class="w-32" />
+    <IonImg
+      :src="`https://cyna-it.fr/wp-content/themes/theme-cyna-it/images/logo-cyna${light ? '-white' : ''}.svg`"
+      class="w-32"
+    />
   </RouterLink>
 </template>
 
 <script setup lang="ts">
 /* Imports */
 import { IonImg } from '@ionic/vue'
+
+/* Props */
+defineProps<{
+  light: boolean
+}>()
 </script>
