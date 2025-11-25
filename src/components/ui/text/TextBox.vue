8825998a117919f067cfd9395c4838b0bc2be813
kpoptrashBlaenk <aldinmusik@gmail.com>
2025-11-25 11:13:55 +0100

    feat(home): add text box to home

---
+++ src/components/ui/text/TextBox.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center gap-5 bg-light p-5">
+    <IonIcon :icon="informationCircleOutline" class="text-9xl" />
+    {{ homeText }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IonIcon } from '@ionic/vue'
+import { informationCircleOutline } from 'ionicons/icons'
+
+/* Props */
+defineProps<{
+  homeText: string
+}>()
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,6 +7,10 @@
     <div class="wrap">
       <SeparatorComponent />
 
+      <TextBox :home-text />
+
+      <SeparatorComponent />
+
       <TitleComponent :text="translation('home_category_title')" color="secondary" />
       <HomeCategoryGrid :categories />
 
@@ -23,23 +27,28 @@ import DefaultLayout from '@/components/layouts/default/DefaultLayout.vue'
 import HomeSwiper from '@/components/swiper/HomeSwiper.vue'
 import HeroComponent from '@/components/ui/HeroComponent.vue'
 import SeparatorComponent from '@/components/ui/SeparatorComponent.vue'
+import TextBox from '@/components/ui/text/TextBox.vue'
 import TitleComponent from '@/components/ui/TitleComponent.vue'
 import { useCategory } from '@/composables/category'
+import { useHomeText } from '@/composables/homeText'
 import { usePromotion } from '@/composables/promotion'
 import translation from '@/utils/translation'
 import { onMounted, ref } from 'vue'
 
 /* Constants */
 const { getPromotions } = usePromotion()
+const { getHomeText } = useHomeText()
 const { getCategories } = useCategory()
 
 /* Refs */
 const promotions = ref<Promotion[]>([])
+const homeText = ref<string>('')
 const categories = ref<Category[]>([])
 
 /* Lifecycle Hooks */
 onMounted(async () => {
   promotions.value = await getPromotions()
+  homeText.value = await getHomeText()
   categories.value = await getCategories()
 })
 </script>
