f4c70a50e0f3455fa6270bec44fe96f92cd02e3d
kpoptrashBlaenk <aldinmusik@gmail.com>
2026-01-15 15:46:14 +0100

    style(product): hide characteristics if none

--- src/components/grids/ProductCharacteristicGrid.vue
+++ src/components/grids/ProductCharacteristicGrid.vue
@@ -1,5 +1,10 @@
 <template>
-  <div v-for="(characteristic, key) in characteristics" :key class="flex">
+  <div
+    v-for="(characteristic, key) in characteristics"
+    :key
+    class="flex"
+    :class="{ hidden: isLengthZero(characteristic.characteristics) }"
+  >
     <div class="w-32 me-5 font-bold text-xl">{{ characteristic.title }}</div>
     <div class="flex flex-wrap gap-1 items-center mb-3">
       <ChipComponent
@@ -17,6 +22,7 @@
 /* Imports */
 import { Characteristic } from '$/types'
 import { Color } from '@/types'
+import isLengthZero from '@/utils/isLengthZero'
 import translation from '@/utils/translation'
 import ChipComponent from '../ui/ChipComponent.vue'
 
