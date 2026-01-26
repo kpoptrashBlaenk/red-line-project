e9b11f26daae2c363a41cf23ba01e535c2e3edd0
kpoptrashBlaenk <aldinmusik@gmail.com>
2026-01-26 09:20:19 +0100

    feat(account): add title

--- src/views/AccountPage.vue
+++ src/views/AccountPage.vue
@@ -1,8 +1,19 @@
 <template>
-  <DefaultContentLayout> </DefaultContentLayout>
+  <DefaultContentLayout>
+    <HeroComponent :title="translation('account')" />
+
+    <div class="wrap">
+      <SeparatorComponent size="sm" />
+
+      <SeparatorComponent size="sm" />
+    </div>
+  </DefaultContentLayout>
 </template>
 
 <script setup lang="ts">
 /* Imports */
 import DefaultContentLayout from '@/components/layouts/default/DefaultContentLayout.vue'
+import HeroComponent from '@/components/ui/HeroComponent.vue'
+import SeparatorComponent from '@/components/ui/SeparatorComponent.vue'
+import translation from '@/utils/translation'
 </script>
