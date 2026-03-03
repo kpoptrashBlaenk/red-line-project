2cd55364eadd873242db78b03eb03e7ffbfaa55e
kpoptrashBlaenk <aldinmusik@gmail.com>
2026-03-03 11:38:30 +0100

    feat(chabot): create fab

---
+++ src/components/chatbot/ChatbotComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <ChatbotFab />
+</template>
+
+<script setup lang="ts">
+/* Imports */
+import ChatbotFab from './ChatbotFab.vue'
+</script>
--- src/components/layouts/default/DefaultLayout.vue
+++ src/components/layouts/default/DefaultLayout.vue
@@ -1,6 +1,7 @@
 <template>
   <IonPage>
     <DefaultMenuLayout />
+    <ChatbotComponent />
 
     <IonRouterOutlet />
   </IonPage>
@@ -8,6 +9,7 @@
 
 <script setup lang="ts">
 /* Imports */
+import ChatbotComponent from '@/components/chatbot/ChatbotComponent.vue'
 import { IonPage, IonRouterOutlet } from '@ionic/vue'
 import DefaultMenuLayout from './DefaultMenuLayout.vue'
 </script>
