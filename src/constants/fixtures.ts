0600fe51b9a362fc1e665e03259967da6da2c2c3
kpoptrashBlaenk <aldinmusik@gmail.com>
2026-03-05 10:26:28 +0100

    feat(admin): hide admin access in menu

--- src/constants/fixtures.ts
+++ src/constants/fixtures.ts
@@ -57,7 +57,7 @@ export const userFixtures: Record<number, User> = {
     email: 'email@email.com',
     phone: '0101010101',
     prefix: '+33',
-    is_admin: true,
+    is_admin: false,
     token: 'ABSOLUTELY_HASHED_TOKEN',
   },
 }
