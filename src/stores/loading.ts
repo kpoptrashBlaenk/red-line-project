705a8c57a59a5a8f9e7615c514bb48da0752439b
kpoptrashBlaenk <aldinmusik@gmail.com>
2026-03-05 11:21:20 +0100

    feat(loading): create loading global

---
+++ src/stores/loading.ts
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+
+export const useLoadingStore = defineStore('loading', {
+  state: () => ({
+    loadingStacks: 0 as number,
+  }),
+
+  getters: {
+    loading: (state) => state.loadingStacks > 0,
+  },
+
+  actions: {
+    addStack() {
+      this.loadingStacks++
+    },
+    popStack() {
+      this.loadingStacks--
+    },
+  },
+})
