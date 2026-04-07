7da14b3c1f3bcefc9a8ee964a23c801edf99e69e
kpoptrashBlaenk <aldinmusik@gmail.com>
2026-04-07 11:20:58 +0200

    fix(server): remove db connection to stop idle server crashes

--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,9 @@
 import dotenv from 'dotenv'
-import app, { pool } from './app'
+import app from './app'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
-
-pool
-  .connect()
-  .then(() => {
-    console.log('Connected to database')
-  })
-  .catch((error: Error) => {
-    console.error('Error connecting to database', error)
-  })
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
